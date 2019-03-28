f1a60c741b3e9759a21fb704e3383067486c3703
olichid <zhengchengc@student.unimelb.edu.au>
2019-03-28 21:08:10 +1100

    add initial file generated from Spring initilizer web tool, include Web, JPA, MySQL and Security dependencies

---
+++ src/main/java/com/olichid/learning/demo_poll/DemoPollApplication.java
@@ -0,0 +1,13 @@
+package com.olichid.learning.demo_poll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olichid/learning/demo_poll/DemoPollApplicationTests.java
@@ -0,0 +1,16 @@
+package com.olichid.learning.demo_poll;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoPollApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
