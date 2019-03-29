fe3664dca290210cdffed155414db51eac3c7624
olichid <zhengchengc@student.unimelb.edu.au>
2019-03-29 23:47:51 +1100

    add the security configuration

---
+++ src/main/java/com/olichid/learning/demo_poll/security/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.olichid.learning.demo_poll.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+}
