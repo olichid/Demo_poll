68b23d391d862219158cdff57540d6575b9f5ade
olichid <zhengchengc@student.unimelb.edu.au>
2019-03-30 16:43:52 +1100

    finish the server side development

---
+++ server-side/src/main/java/com/olichid/learning/demo_poll/payload/VoteRequest.java
@@ -0,0 +1,16 @@
+package com.olichid.learning.demo_poll.payload;
+
+import javax.validation.constraints.NotNull;
+
+public class VoteRequest {
+    @NotNull
+    private Long choiceId;
+
+    public Long getChoiceId() {
+        return choiceId;
+    }
+
+    public void setChoiceId(Long choiceId) {
+        this.choiceId = choiceId;
+    }
+}
