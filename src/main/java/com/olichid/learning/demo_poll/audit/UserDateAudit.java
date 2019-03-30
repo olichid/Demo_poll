d506bac86a0e9dd5f72e6d56f7680db9f13a4b55
olichid <zhengchengc@student.unimelb.edu.au>
2019-03-30 14:15:09 +1100

    add poll and vote action

---
+++ src/main/java/com/olichid/learning/demo_poll/audit/UserDateAudit.java
@@ -0,0 +1,40 @@
+package com.olichid.learning.demo_poll.audit;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.olichid.learning.demo_poll.model.audit.DateAudit;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@JsonIgnoreProperties(
+        value = {"createdBy", "updatedBy"},
+        allowGetters = true
+)
+public abstract class UserDateAudit extends DateAudit {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    private Long updatedBy;
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Long getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Long updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+}
